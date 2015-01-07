6dcee8fa6f43d1dcf9ef1c72651dcff554e01b5e
nicolaiparlog <nipa@codefx.org>
2015-01-07 12:07:07 +0100

    Let the plans with 'Lottery.pickWinners' uphold semantic versioning

--- src/main/java/org/codefx/demo/javadoc8tags/Lottery.java
+++ src/main/java/org/codefx/demo/javadoc8tags/Lottery.java
@@ -19,9 +19,9 @@ public interface Lottery {
 	 * The returned list defines the order of the winners, where the first prize goes to the player at position 0. The
 	 * list will not be null but can be empty.
 	 *
-	 * @apiNote This method was added after the interface was released. It is defined as a default method for
-	 *          compatibility reasons. From version 1.2 on, the method will be abstract and all implementations of this
-	 *          interface have to provide their own implementation of the method.
+	 * @apiNote This method was added after the interface was released in version 1.0. It is defined as a default method
+	 *          for compatibility reasons. From version 2.0 on, the method will be abstract and all implementations of
+	 *          this interface have to provide their own implementation of the method.
 	 * @implSpec The default implementation will consider each player a winner and return them in an unspecified order.
 	 * @implNote This implementation has linear runtime and does not filter out null players.
 	 * @param players
